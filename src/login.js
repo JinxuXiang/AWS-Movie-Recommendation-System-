fd0a535ba6cc9419677fbc88871378eb66d6ba4d
JinxuXiang <jx2399@columbia.edu>
2021-04-13 15:33:33 -0400

    upload

--- src/login.js
+++ src/login.js
@@ -1,5 +1,7 @@
+
+
 function login() {
- 
+    var user_csv = {Bucket: storage_bucket, Key: 'user_pro.csv'};
     var username = document.getElementById("username_login");
     var pass = document.getElementById("password_login");
  
@@ -11,13 +13,27 @@ function login() {
  
         alert("Please enter password!");
  
-    } else if(username.value == "g49xP1hK8P" && pass.value == "dw2mt76z9G"){
- 
-        window.location.href="user.html?user="+username.value+"&pass="+pass.value;
- 
-    } else {
- 
-        alert("Please enter the correct username and password!")
- 
-    }
+    } 
+    var get_user = new AWS.S3().getObject(user_csv, function(err, data)
+    {
+        if (!err){
+            csv_data = parseData(data.Body.toString('utf-8'))
+            Find_user = false
+            
+            for (var i = 0; i < csv_data.length; i++) {
+                console.log(csv_data[i][2], username)
+                console.log(csv_data[i][3], pass)
+                if (csv_data[i][2] == username.value && csv_data[i][3] == pass.value){
+                    
+                    window.location.href="user.html?user="+username.value+"&pass="+pass.value;
+                    Find_user = true
+                    break
+                }
+            }
+            if (!Find_user){
+                alert("Please enter the correct username and password!")
+            }
+        }        
+    })
+
 }
